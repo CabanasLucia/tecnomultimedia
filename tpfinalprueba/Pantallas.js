376d7b099b620c03a79d3d1b1e7bc737939a577f
CabanasLucia <128551161+CabanasLucia@users.noreply.github.com>
2023-11-16 20:08:42 -0300

    Add files via upload

--- tpfinalprueba/Pantallas.js
+++ tpfinalprueba/Pantallas.js
@@ -7,14 +7,15 @@ class Pantallas {
 load (){
    this.imagenes = [];
     
-   for (let i = 0; i < 14; i++) {
+   for (let i = 0; i < 15; i++) {
       this.imagenes[i] = loadImage("data/imagen"+i+".jpg");
    }
       
 }
  
-   dibujarPantalla(numero, texto) {
-    return this.imagenes[numero]; // Devuelve la imagen correspondiente al número de pantalla
+   dibujarPantalla(numero) {
+    return this.imagenes[numero]; // --- DEVUELVE LA IMAGEN CORRESPONDIENTE AL NRO DE PANTALLA ---
   }
+  
   
 }
--- tpfinalprueba/tpfinalprueba.js
+++ tpfinalprueba/tpfinalprueba.js
@@ -3,16 +3,33 @@
 
 
 let principal;
+let personaje; 
+let autito;
+
+function preload (){
+  autito = loadImage ('data/auto.png');
+  personaje = loadImage ('data/personaje.png');
+  
+}
 
 function setup() {
   createCanvas(600, 600);
   principal = new Principal();
+ 
+  
 }
 
 function draw() {
   background(100);
   principal.dibujar();
 }
+
+function keyPressed() {
+  
+  principal.moverPersonaje();
+ 
+}
+
 function mousePressed() {
   principal.accionarBotones();
 }

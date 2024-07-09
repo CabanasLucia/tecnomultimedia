b90de26fc26444c20d283de5645445bcfb5809ee
CabanasLucia <128551161+CabanasLucia@users.noreply.github.com>
2024-07-09 18:45:41 -0300

    Delete entregafinal2 directory

--- entregafinal2/Imagenes.js
+++
@@ -1,9 +0,0 @@
-class Imagenes {
-    constructor() {
-        this.manchas = [];
-        for (let i = 1; i < 27; i++) {
-            this.manchas.push(loadImage("data/mancha" + i + ".png"));
-        }
-        this.fondoAmarillo = loadImage("data/fondoAmarillo.png");
-    }
-}
